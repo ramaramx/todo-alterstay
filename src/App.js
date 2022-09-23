707f01f252c2bf2a07085077ef006f5939bdf7d1
ramaramx <rambudiarto@gmail.com>
2022-09-23 15:49:16 +0700

    new

---
+++ src/App.js
@@ -0,0 +1,241 @@
+import {
+	Button,
+	Container,
+	Text,
+	Title,
+	Modal,
+	TextInput,
+	Group,
+	Card,
+	ActionIcon,
+	Checkbox,
+} from '@mantine/core';
+import { useState, useRef, useEffect } from 'react';
+import { MoonStars, Sun, Trash } from 'tabler-icons-react';
+
+import {
+	MantineProvider,
+	ColorSchemeProvider,
+} from '@mantine/core';
+import { useHotkeys, useLocalStorage } from '@mantine/hooks';
+
+export default function App() {
+	const [tasks, setTasks] = useState([]);
+	const [opened, setOpened] = useState(false);
+
+	const [colorScheme, setColorScheme] = useLocalStorage({
+		key: 'mantine-color-scheme',
+		defaultValue: 'light',
+		getInitialValueInEffect: true,
+	});
+	const toggleColorScheme = value =>
+		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+	useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+	const taskTitle = useRef('');
+	const propertyType = useRef('');
+	const numberRooms = useRef('');
+	const link = useRef('');
+	const taskSummary = useRef('');
+
+	function createTask() {
+		setTasks([
+			...tasks,
+			{
+				title: taskTitle.current.value,
+				propertyType: propertyType.current.value,
+				numberRooms: numberRooms.current.value,
+				link: link.current.value,
+				summary: taskSummary.current.value,
+			},
+		]);
+
+		saveTasks([
+			...tasks,
+			{
+				title: taskTitle.current.value,
+				propertyType: propertyType.current.value,
+				numberRooms: numberRooms.current.value,
+				link: link.current.value,
+				summary: taskSummary.current.value,
+			},
+		]);
+	}
+
+	function deleteTask(index) {
+		var clonedTasks = [...tasks];
+
+		clonedTasks.splice(index, 1);
+
+		setTasks(clonedTasks);
+
+		saveTasks([...clonedTasks]);
+	}
+
+	function loadTasks() {
+		let loadedTasks = localStorage.getItem('tasks');
+
+		let tasks = JSON.parse(loadedTasks);
+
+		if (tasks) {
+			setTasks(tasks);
+		}
+	}
+
+	function saveTasks(tasks) {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}
+
+	useEffect(() => {
+		loadTasks();
+	}, []);
+
+	return (
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}>
+			<MantineProvider
+				theme={{ colorScheme, defaultRadius: 'md' }}
+				withGlobalStyles
+				withNormalizeCSS>
+				<div className='App'>
+					<Modal
+						opened={opened}
+						size={'md'}
+						title={'New Task'}
+						withCloseButton={false}
+						onClose={() => {
+							setOpened(false);
+						}}
+						centered>
+						<TextInput
+							mt={'md'}
+							ref={taskTitle}
+							placeholder={'Task Title'}
+							required
+							label={'Title'}
+						/>
+						<TextInput
+							mt={'md'}
+							ref={propertyType}
+							placeholder={'property type'}
+							required
+							label={'property type'}
+						/>
+						<TextInput
+							mt={'md'}
+							ref={numberRooms}
+							placeholder={'number of rooms'}
+							required
+							label={'number of rooms'}
+						/>
+						<TextInput
+							mt={'md'}
+							ref={link}
+							placeholder={'google drive link'}
+							required
+							label={'link'}
+						/>
+						<TextInput
+							mt={'md'}
+							ref={taskSummary}
+							placeholder={'Task Summary'}
+							label={'Summary'}
+						/>
+						<Group mt={'md'} position={'apart'}>
+							<Button
+								onClick={() => {
+									setOpened(false);
+								}}
+								variant={'subtle'}>
+								Cancel
+							</Button>
+							<Button
+								onClick={() => {
+									createTask();
+									setOpened(false);
+								}}>
+								Create Task
+							</Button>
+						</Group>
+					</Modal>
+					<Container size={550} my={40}>
+						<Group position={'apart'}>
+							<Title
+								sx={theme => ({
+									fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+									fontWeight: 900,
+								})}>
+								Pre-Opening Checklist
+							</Title>
+							<ActionIcon
+								color={'blue'}
+								onClick={() => toggleColorScheme()}
+								size='lg'>
+								{colorScheme === 'dark' ? (
+									<Sun size={16} />
+								) : (
+									<MoonStars size={16} />
+								)}
+							</ActionIcon>
+						</Group>
+						{tasks.length > 0 ? (
+							// eslint-disable-next-line array-callback-return
+							tasks.map((task, index) => {
+								if (task.title) {
+									return (
+										<Card withBorder key={index} mt={'sm'}>
+											<Group position={'apart'}>
+												<Text weight={'bold'}>{task.title}</Text>
+												
+												
+												
+												<ActionIcon
+													onClick={() => {
+														deleteTask(index);
+													}}
+													color={'red'}
+													variant={'transparent'}>
+													<Trash />
+												</ActionIcon>
+											</Group>
+											<Text weight={'bold'}>{task.propertyType}</Text>
+											<Text weight={'bold'}>{task.numberRooms}</Text>
+											<Text weight={'bold'}>{task.link}</Text>
+											<Text color={'dimmed'} size={'md'} mt={'sm'}>
+												{task.summary
+													? task.summary
+													: 'No summary was provided for this task'}
+											</Text>
+											<Checkbox
+												mt="md"
+												label="guest pro"
+												/>
+												<Checkbox
+												mt="md"
+												label="photo"
+												/>
+										</Card>
+									);
+								}
+							})
+						) : (
+							<Text size={'lg'} mt={'md'} color={'dimmed'}>
+								You have no tasks
+							</Text>
+						)}
+						<Button
+							onClick={() => {
+								setOpened(true);
+							}}
+							fullWidth
+							mt={'md'}>
+							New Task
+						</Button>
+					</Container>
+				</div>
+			</MantineProvider>
+		</ColorSchemeProvider>
+	);
+}
